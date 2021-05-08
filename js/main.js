f8ff6512030cfa01c609f4c7e69c86925981498e
Rohit Parakh <rohitparakh4@gmail.com>
2021-05-08 20:06:57 +0530

    Fetching latest three blog posts complete

--- js/main.js
+++ js/main.js
@@ -1,80 +1,10 @@
+
+
+
 (function($) {
     "use strict";
 
-	const projects=[
-		{
-			id:1,
-			name:"Teenprenuer Merch",
-			href:"https://merch.teenpreneur.co",
-			imgSrc:"images/work/merchtp.png",
-			description:"Entrepreneur Merchandise",
-			category:"Shopify Development"
-		},
-		{
-			id:2,
-			name:"Luxewalk",
-			href:"https://www.luxewalk.com",
-			imgSrc:"images/work/luxewalk.png",
-			description:"Natural Candles E-Com Store",
-			category:"Shopify Development"
-		},
-		{
-			id:3,
-			name:"ExpliciveX",
-			href:"https://www.explicivex.com",
-			imgSrc:"images/work/explicivex.jpeg",
-			description:"Digital Marketing Agency",
-			category:"Wordpress Development"
-		},
-		{
-			id:4,
-			name:"Teenprenuer Blog",
-			href:"https://www.teenpreneur.co",
-			imgSrc:"images/work/teenpreneur.JPG",
-			description:"Teenage Entreprenuer Blog",
-			category:"Wordpress Development"
-		},
-		{
-			id:5,
-			name:"SVM Electronics",
-			href:"https://rohitparakh.github.io/svm-react",
-			imgSrc:"images/work/svm-react.png",
-			description:"LG Retail Showroom",
-			category:"React Development"
-		},
-		{
-			id:6,
-			name:"Paranoia Miami",
-			href:"https://paranoiamiami.com/",
-			imgSrc:"images/work/paranoia.png",
-			description:"Award Winning Horror Maze based out in Miami, Florida, USA",
-			category:"Wordpress Development"
-		},
-		{
-			id:7,
-			name:"Mobile App Website",
-			href:"https://rohit-mobile-app.netlify.app",
-			imgSrc:"images/work/mobileapp.png",
-			description:"Project Website made for Mobile Application",
-			category:"Website Development"
-		},
-		{
-			id:8,
-			name:"Youtube Clone",
-			href:"https://rohitparakh.github.io/rohit-ytc",
-			imgSrc:"images/work/ytc.png",
-			description:"Youtube clone made using Youtube Data API v3",
-			category:"React Development"
-		},
-		{
-			id:9,
-			name:"Interior Decorators",
-			href:"https://rohit-decorators.netlify.app/",
-			imgSrc:"images/work/decorators.PNG",
-			description:"Project made for non-existent Interior Decorators Store",
-			category:"Website Development"
-		}
-	]
+	
 
 	// Portfolio Websites Display
 
@@ -104,12 +34,11 @@ const displayPortfolio=(projects)=>{
 
 }
 
-
-	
-	
+displayPortfolio(projectsData)
 
 
-displayPortfolio(projects)
+var a=findLatestThreePosts(blogData)
+console.log(a)	
 
 	/*----------------------------
     START - Menubar scroll animation
@@ -516,6 +445,3 @@ function arlo_tm_contact_form(){
 		return false; 
 	});
 }
-
-var now=moment().format("12-25-1995",'DD MM YYYY')
-console.log(now)
