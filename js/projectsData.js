f8542f3a9dcc95e99dd7459c7a1698b59bd52702
Rohit Parakh <rohitparakh4@gmail.com>
2021-10-10 14:33:27 +0530

    Changed projects order

--- js/projectsData.js
+++ js/projectsData.js
@@ -1,118 +1,6 @@
 const projectsData=[
     {
         id:1,
-        name:"Teenprenuer Merch",
-        href:"https://merch.teenpreneur.co",
-        imgSrc:"images/work/merchtp.png",
-        description:"Entrepreneur Merchandise",
-        category:"Shopify Development"
-    },
-    {
-        id:2,
-        name:"Luxewalk",
-        href:"https://www.luxewalk.com",
-        imgSrc:"images/work/luxewalk.png",
-        description:"Natural Candles E-Com Store",
-        category:"Shopify Development"
-    },
-    {
-        id:3,
-        name:"ExpliciveX",
-        href:"https://www.explicivex.com",
-        imgSrc:"images/work/explicivex.jpeg",
-        description:"Digital Marketing Agency",
-        category:"Wordpress Development"
-    },
-    {
-        id:4,
-        name:"Teenprenuer Blog",
-        href:"https://www.teenpreneur.co",
-        imgSrc:"images/work/teenpreneur.JPG",
-        description:"Teenage Entreprenuer Blog",
-        category:"Wordpress Development"
-    },
-    {
-        id:5,
-        name:"SVM Electronics",
-        href:"https://rohitparakh.github.io/svm-react",
-        imgSrc:"images/work/svm-react.png",
-        description:"LG Retail Showroom",
-        category:"React Development"
-    },
-    {
-        id:6,
-        name:"Paranoia Miami",
-        href:"https://paranoiamiami.com/",
-        imgSrc:"images/work/paranoia.png",
-        description:"Award Winning Horror Maze based out in Miami, Florida, USA",
-        category:"Wordpress Development"
-    },
-    {
-        id:7,
-        name:"Mobile App Website",
-        href:"https://rohit-mobile-app.netlify.app",
-        imgSrc:"images/work/mobileapp.png",
-        description:"Project Website made for Mobile Application",
-        category:"Website Development"
-    },
-    {
-        id:8,
-        name:"Youtube Clone",
-        href:"https://rohitparakh.github.io/rohit-ytc",
-        imgSrc:"images/work/ytc.png",
-        description:"Youtube clone made using Youtube Data API v3",
-        category:"React Development"
-    },
-    {
-        id:9,
-        name:"Interior Decorators",
-        href:"https://rohit-decorators.netlify.app/",
-        imgSrc:"images/work/decorators.PNG",
-        description:"Project made for non-existent Interior Decorators Store",
-        category:"Website Development"
-    },
-    {
-        id:10,
-        name:"HyperIllusion",
-        href:"https://www.hyperillusion.in",
-        imgSrc:"images/work/HyperIllusion.png",
-        description:"Street Culture Fashion Brand",
-        category:"Shopify Development"
-    },
-    {
-        id:11,
-        name:"The Drip Story",
-        href:"https://www.thedripstory.com",
-        imgSrc:"images/work/TheDripStory.png",
-        description:"Digital Service Agency",
-        category:"Wordpress Development"
-    },
-    {
-        id:12,
-        name:"Witty Spirit",
-        href:"https://www.wittyspirit.com",
-        imgSrc:"images/work/WittySpirit.png",
-        description:"Fashion Brand",
-        category:"Shopify Development"
-    },
-    {
-        id:13,
-        name:"SpaceX by Rohit",
-        href:"https://www.spacex-herokuapp.com",
-        imgSrc:"images/work/SpaceXRohit.png",
-        description:"SpaceX launches made using SpaceX API",
-        category:"React Development"
-    },
-    {
-        id:14,
-        name:"Phohit",
-        href:"https://rohit-photography.netlify.app/",
-        imgSrc:"images/work/Phohit.png",
-        description:"Photography portfolio website",
-        category:"Website Development"
-    },
-    {
-        id:15,
         name:"Kay Store",
         href:"https://kaystore.in/",
         imgSrc:"images/work/kayStore.png",
@@ -120,13 +8,125 @@ const projectsData=[
         category:"Shopify Development"
     },
     {
-        id:16,
+        id:2,
+        name:"SpaceX by Rohit",
+        href:"https://www.spacex-herokuapp.com",
+        imgSrc:"images/work/SpaceXRohit.png",
+        description:"SpaceX launches made using SpaceX API",
+        category:"React Development"
+    },
+    {
+        id:3,
+        name:"The Drip Story",
+        href:"https://www.thedripstory.com",
+        imgSrc:"images/work/TheDripStory.png",
+        description:"Digital Service Agency",
+        category:"Wordpress Development"
+    },
+    {
+        id:4,
+        name:"Interior Decorators",
+        href:"https://rohit-decorators.netlify.app/",
+        imgSrc:"images/work/decorators.PNG",
+        description:"Project made for non-existent Interior Decorators Store",
+        category:"Website Development"
+    },
+    {
+        id:5,
         name:"Kay Desi",
         href:"https://kaydesi.in",
         imgSrc:"images/work/kayDesi.png",
         description:"Indian Clothing store",
         category:"Shopify Development"
     },
+    {
+        id:6,
+        name:"SVM Electronics",
+        href:"https://rohitparakh.github.io/svm-react",
+        imgSrc:"images/work/svm-react.png",
+        description:"LG Retail Showroom",
+        category:"React Development"
+    },
+    {
+        id:7,
+        name:"Paranoia Miami",
+        href:"https://paranoiamiami.com/",
+        imgSrc:"images/work/paranoia.png",
+        description:"Award Winning Horror Maze based out in Miami, Florida, USA",
+        category:"Wordpress Development"
+    },
+    {
+        id:8,
+        name:"Mobile App Website",
+        href:"https://rohit-mobile-app.netlify.app",
+        imgSrc:"images/work/mobileapp.png",
+        description:"Project Website made for Mobile Application",
+        category:"Website Development"
+    },
+    {
+        id:9,
+        name:"Luxewalk",
+        href:"https://www.luxewalk.com",
+        imgSrc:"images/work/luxewalk.png",
+        description:"Natural Candles E-Com Store",
+        category:"Shopify Development"
+    },
+    {
+        id:10,
+        name:"Youtube Clone",
+        href:"https://rohitparakh.github.io/rohit-ytc",
+        imgSrc:"images/work/ytc.png",
+        description:"Youtube clone made using Youtube Data API v3",
+        category:"React Development"
+    },
+    {
+        id:11,
+        name:"ExpliciveX",
+        href:"https://www.explicivex.com",
+        imgSrc:"images/work/explicivex.jpeg",
+        description:"Digital Marketing Agency",
+        category:"Wordpress Development"
+    },
+    {
+        id:12,
+        name:"Phohit",
+        href:"https://rohit-photography.netlify.app/",
+        imgSrc:"images/work/Phohit.png",
+        description:"Photography portfolio website",
+        category:"Website Development"
+    },
+    {
+        id:13,
+        name:"Teenprenuer Merch",
+        href:"https://merch.teenpreneur.co",
+        imgSrc:"images/work/merchtp.png",
+        description:"Entrepreneur Merchandise",
+        category:"Shopify Development"
+    },
+    {
+        id:14,
+        name:"HyperIllusion",
+        href:"https://www.hyperillusion.in",
+        imgSrc:"images/work/HyperIllusion.png",
+        description:"Street Culture Fashion Brand",
+        category:"Shopify Development"
+    },
+    {
+        id:15,
+        name:"Witty Spirit",
+        href:"https://www.wittyspirit.com",
+        imgSrc:"images/work/WittySpirit.png",
+        description:"Fashion Brand",
+        category:"Shopify Development"
+    },  
+    {
+        id:16,
+        name:"Teenprenuer Blog",
+        href:"https://www.teenpreneur.co",
+        imgSrc:"images/work/teenpreneur.JPG",
+        description:"Teenage Entreprenuer Blog",
+        category:"Wordpress Development"
+    }, 
     {
         id:17,
         name:"The Gifts Basket",
