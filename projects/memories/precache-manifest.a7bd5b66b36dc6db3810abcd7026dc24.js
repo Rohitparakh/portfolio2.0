d6f5f6b39c7ceeace73f63df79fd7c40cf03e02f
Rohit Parakh <rohitparakh4@gmail.com>
2022-03-08 19:20:18 +0530

    Update route final memories

---
+++ projects/memories/precache-manifest.a7bd5b66b36dc6db3810abcd7026dc24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e344eabde29169169868d89ed06f506d",
+    "url": "/projects/memories/index.html"
+  },
+  {
+    "revision": "d27b934d108c428b1869",
+    "url": "/projects/memories/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "305d16d27a6d4aa2bad9",
+    "url": "/projects/memories/static/js/2.00ed743a.chunk.js"
+  },
+  {
+    "revision": "355ef2daa397c9d2d6b317bfe49025af",
+    "url": "/projects/memories/static/js/2.00ed743a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27b934d108c428b1869",
+    "url": "/projects/memories/static/js/main.fa16dc80.chunk.js"
+  },
+  {
+    "revision": "171efc952fbeb8cc268d",
+    "url": "/projects/memories/static/js/runtime-main.a7141588.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/projects/memories/static/media/memories.5c0c63fb.png"
+  }
+]);
--- projects/memories/service-worker.js
+++ projects/memories/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/memories/precache-manifest.10afd830f452bab43b52d71fe784d0d5.js"
+  "/projects/memories/precache-manifest.a7bd5b66b36dc6db3810abcd7026dc24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memories/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/memories/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
