8c0826abb978185f0b47271445bceba5392bf55a
Rohit Parakh <rohitparakh4@gmail.com>
2022-03-08 19:08:03 +0530

    Updating urls memories project

--- projects/memories/precache-manifest.10afd830f452bab43b52d71fe784d0d5.js
+++ projects/memories/precache-manifest.10afd830f452bab43b52d71fe784d0d5.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "1d3c329a85fcea97275d84ac00273b61",
-    "url": "/memories/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "4b2ecfc1145d1cc3354e",
-    "url": "/memories/static/css/main.a1d481cb.chunk.css"
+    "url": "./static/css/main.a1d481cb.chunk.css"
   },
   {
     "revision": "1ac29e6c6f79833eda9b",
-    "url": "/memories/static/js/2.1deee530.chunk.js"
+    "url": "./static/js/2.1deee530.chunk.js"
   },
   {
     "revision": "57e95f828356cddffcdadcd36cab5b13",
-    "url": "/memories/static/js/2.1deee530.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.1deee530.chunk.js.LICENSE.txt"
   },
   {
     "revision": "4b2ecfc1145d1cc3354e",
-    "url": "/memories/static/js/main.dd073a77.chunk.js"
+    "url": "./static/js/main.dd073a77.chunk.js"
   },
   {
     "revision": "e0270bc638061f027bc0",
-    "url": "/memories/static/js/runtime-main.1975d964.js"
+    "url": "./static/js/runtime-main.1975d964.js"
   },
   {
     "revision": "5c0c63fb62ec323a10598d7e9de4609e",
-    "url": "/memories/static/media/memories.5c0c63fb.png"
+    "url": "./static/media/memories.5c0c63fb.png"
   }
 ]);
