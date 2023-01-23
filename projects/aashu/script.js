474373a1970c85cf8b7405c9770c937590f651c0
Rohit Parakh <rohitparakh4@gmail.com>
2023-01-23 11:40:31 +0530

    Changed to slick slider

--- projects/aashu/script.js
+++ projects/aashu/script.js
@@ -50,3 +50,17 @@ Spanizer
 })(jQuery);
 
 
+$('#slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  draggable:true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnHover:false,
+  pauseOnFocus:false,
+  prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+});
