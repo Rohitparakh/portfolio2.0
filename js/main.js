773ba23cd9a0be588194b643613ea605bb8adb5f
Rohit Parakh <62591195+Rohitparakh@users.noreply.github.com>
2024-03-06 16:15:08 +0530

    Removed the blog functions

--- js/main.js
+++ js/main.js
@@ -8,88 +8,88 @@ var pathname=window.location.pathname;
 
 
 
-const findLatestThreePosts=(blogJSON)=>{
+// const findLatestThreePosts=(blogJSON)=>{
  
-    var blogsWithRequiredDate=[];
-    var blogsWithoutRequiredDate=blogJSON;
-        for (let i = 0; i < 3; i++) {
+//     var blogsWithRequiredDate=[];
+//     var blogsWithoutRequiredDate=blogJSON;
+//         for (let i = 0; i < 3; i++) {
                   
-            // Finding the latest blog object in the array
-            var highestDatedBlog=blogsWithoutRequiredDate.reduce((a, b) => {
-                return moment(a.date,"MMM DD, YYYY") > moment(b.date,"MMM DD, YYYY") ? a : b;
-                console.log(a)
-                console.log(b)
-              })
+//             // Finding the latest blog object in the array
+//             var highestDatedBlog=blogsWithoutRequiredDate.reduce((a, b) => {
+//                 return moment(a.date,"MMM DD, YYYY") > moment(b.date,"MMM DD, YYYY") ? a : b;
+//                 console.log(a)
+//                 console.log(b)
+//               })
     
-            //   Pushing the latest blog object to an array 
-            blogsWithRequiredDate.push(highestDatedBlog)
+//             //   Pushing the latest blog object to an array 
+//             blogsWithRequiredDate.push(highestDatedBlog)
     
-            //   Filtering an array of blog objects without the latest dated blog
-            blogsWithoutRequiredDate=blogsWithoutRequiredDate.filter(e => {
-                return e.date !=highestDatedBlog.date
-            });
+//             //   Filtering an array of blog objects without the latest dated blog
+//             blogsWithoutRequiredDate=blogsWithoutRequiredDate.filter(e => {
+//                 return e.date !=highestDatedBlog.date
+//             });
     
-    }
-    console.log({blogsWithRequiredDate})
-        return blogsWithRequiredDate
-    }
+//     }
+//     console.log({blogsWithRequiredDate})
+//         return blogsWithRequiredDate
+//     }
     
-const latestBlogNavigatorRenderer=(blogJSON,idToBeRenderedAt)=>{
-		var blogNavigator=blogJSON.map((value,index)=>{
-			var activeClass=index==0?"active":"";
+// const latestBlogNavigatorRenderer=(blogJSON,idToBeRenderedAt)=>{
+// 		var blogNavigator=blogJSON.map((value,index)=>{
+// 			var activeClass=index==0?"active":"";
 	
-			return `<li data-target='#quote-carousel' data-slide-to='${index}' class="${activeClass}"><img class="img-responsive" src="${value.featuredImage}" alt="">
-			</li>
-			`
-		}).join("")
-		if(document.getElementById("latestBlogCarouselIndicator"))document.getElementById("latestBlogCarouselIndicator").innerHTML=blogNavigator
-	console.log({idToBeRenderedAt})
-	}
+// 			return `<li data-target='#quote-carousel' data-slide-to='${index}' class="${activeClass}"><img class="img-responsive" src="${value.featuredImage}" alt="">
+// 			</li>
+// 			`
+// 		}).join("")
+// 		if(document.getElementById("latestBlogCarouselIndicator"))document.getElementById("latestBlogCarouselIndicator").innerHTML=blogNavigator
+// 	console.log({idToBeRenderedAt})
+// 	}
 
-	const latestBlogRenderer=(blogJSON,idToBeRenderedAt)=>{
-		var allBlogs=null;
-		{allBlogs=blogJSON.map((blog,index)=>{
-			var blogDate=moment(blog.date,"MMM DD, YYYY").format("DD")
-			var blogMonth=moment(blog.date,"MMM DD, YYYY").format("MMM")
-			var blogYear=moment(blog.date,"MMM DD, YYYY").format("YYYY")
-			var activeClass=index==1?"active":"";
-			return  `<div class="item ${activeClass}">
-								<div class="row">
-								<div class="col-md-5 col-sm-9">
-									<div class="blog-left">
-									  <a href="/blog-post.html?id=${blog.id}">
-										<img src=${blog.latestImgSrc} alt="" /></a>
-									</div>
-								</div>
-								<div class="col-md-1 col-sm-3">
-									<div class="blog-comment">
-										<h1>${blogDate}</h1>
-										<h4>${blogMonth}, ${blogYear}</h4>
-										<h1>21</h1>
-										<h4>Comm</h4>
-									</div>
-								</div>
-								<div class="col-md-6 col-sm-12">
-									<div class="blog-right">
-									<a href="/blog-post.html?id=${blog.id}">  
-									<h2>${blog.title}</h2>
-									</a>
-										<i class="fa fa-folder-open" aria-hidden="true"></i><span>  ${blog.category.map((category,index)=>{
-											return `${category}`
-										}).join(", ")}</span>
-										<p>${blog.latestBlogDesc}</p>
-									</div>
-								</div>
-							</div>
-						</div>`
-		}).join('')
-		if(document.getElementById(idToBeRenderedAt))document.getElementById(idToBeRenderedAt).innerHTML=allBlogs
+	// const latestBlogRenderer=(blogJSON,idToBeRenderedAt)=>{
+	// 	var allBlogs=null;
+	// 	{allBlogs=blogJSON.map((blog,index)=>{
+	// 		var blogDate=moment(blog.date,"MMM DD, YYYY").format("DD")
+	// 		var blogMonth=moment(blog.date,"MMM DD, YYYY").format("MMM")
+	// 		var blogYear=moment(blog.date,"MMM DD, YYYY").format("YYYY")
+	// 		var activeClass=index==1?"active":"";
+	// 		return  `<div class="item ${activeClass}">
+	// 							<div class="row">
+	// 							<div class="col-md-5 col-sm-9">
+	// 								<div class="blog-left">
+	// 								  <a href="/blog-post.html?id=${blog.id}">
+	// 									<img src=${blog.latestImgSrc} alt="" /></a>
+	// 								</div>
+	// 							</div>
+	// 							<div class="col-md-1 col-sm-3">
+	// 								<div class="blog-comment">
+	// 									<h1>${blogDate}</h1>
+	// 									<h4>${blogMonth}, ${blogYear}</h4>
+	// 									<h1>21</h1>
+	// 									<h4>Comm</h4>
+	// 								</div>
+	// 							</div>
+	// 							<div class="col-md-6 col-sm-12">
+	// 								<div class="blog-right">
+	// 								<a href="/blog-post.html?id=${blog.id}">  
+	// 								<h2>${blog.title}</h2>
+	// 								</a>
+	// 									<i class="fa fa-folder-open" aria-hidden="true"></i><span>  ${blog.category.map((category,index)=>{
+	// 										return `${category}`
+	// 									}).join(", ")}</span>
+	// 									<p>${blog.latestBlogDesc}</p>
+	// 								</div>
+	// 							</div>
+	// 						</div>
+	// 					</div>`
+	// 	}).join('')
+	// 	if(document.getElementById(idToBeRenderedAt))document.getElementById(idToBeRenderedAt).innerHTML=allBlogs
 	
-	}
-	console.log({idToBeRenderedAt})
-	if(document.getElementById("latestBlogCarousel"))document.getElementById("latestBlogCarousel").innerHTML=allBlogs
+	// }
+	// console.log({idToBeRenderedAt})
+	// if(document.getElementById("latestBlogCarousel"))document.getElementById("latestBlogCarousel").innerHTML=allBlogs
 
-	}
+	// }
 
 	// Portfolio Websites Display
 	var latestBlogJSON
